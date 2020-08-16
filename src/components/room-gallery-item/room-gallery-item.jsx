1726d6fdabd7190263d22ee708fc28c31bf3fb45
daniel-mironenko <dm25an22@gmail.com>
2020-08-16 14:46:26 +0200

    add RoomGalleryItemComponent

---
+++ src/components/room-gallery-item/room-gallery-item.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RoomGalleryItem = () => {
+  return (
+    <div className="property__image-wrapper">
+      <img className="property__image" src="img/room.jpg" alt="Photo studio" />
+    </div>
+  );
+};
+
+export default RoomGalleryItem;
--- src/components/room-gallery/room-gallery.jsx
+++ src/components/room-gallery/room-gallery.jsx
@@ -1,28 +1,11 @@
-/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from "react";
+import RoomGalleryItem from "../room-gallery-item/room-gallery-item";
 
 const RoomGallery = () => {
   return (
     <div className="property__gallery-container container">
     <div className="property__gallery">
-      <div className="property__image-wrapper">
-        <img className="property__image" src="img/room.jpg" alt="Photo studio" />
-      </div>
-      <div className="property__image-wrapper">
-        <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio" />
-      </div>
-      <div className="property__image-wrapper">
-        <img className="property__image" src="img/apartment-02.jpg" alt="Photo studio" />
-      </div>
-      <div className="property__image-wrapper">
-        <img className="property__image" src="img/apartment-03.jpg" alt="Photo studio" />
-      </div>
-      <div className="property__image-wrapper">
-        <img className="property__image" src="img/studio-01.jpg" alt="Photo studio" />
-      </div>
-      <div className="property__image-wrapper">
-        <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio" />
-      </div>
+      <RoomGalleryItem />
     </div>
   </div>
   );
