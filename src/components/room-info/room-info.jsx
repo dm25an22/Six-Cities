f80d403682b5d215862294578d3d4fa58ca61faa
daniel-mironenko <dm25an22@gmail.com>
2020-08-23 11:41:15 +0200

    fix bookmark in room-info

--- src/components/room-info/room-info.jsx
+++ src/components/room-info/room-info.jsx
@@ -1,14 +1,21 @@
 import React, { useContext } from "react";
 import { ContextRoom } from "../../context";
-import { getRatingByPercent } from "../../utils";
+import { getRatingByPercent, checkInFavorites } from "../../utils";
 import RoomInsideList from "../room-inside-list/room-inside-list";
 import RoomHost from "../room-host/room-host";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { getAuthStatus } from "../../reducer/userReducer/selector";
+import { getFavorites } from "../../reducer/favoritesReducer/selector";
+import { Operation } from "../../reducer/favoritesReducer/favoritesReducer";
 
 const RoomInfo = () => {
   const { hotel } = useContext(ContextRoom);
+  const dispatch = useDispatch();
   const authStatus = useSelector(getAuthStatus);
+  const favorites = useSelector(getFavorites);
+  const isFavorite = checkInFavorites(favorites, hotel.id);
+
+  const status = isFavorite ? 0 : 1;
 
   const {
     isPremium,
@@ -21,6 +28,7 @@ const RoomInfo = () => {
     goods,
     host,
     description,
+    id
   } = hotel;
 
   return (
@@ -35,8 +43,10 @@ const RoomInfo = () => {
         <h1 className="property__name">{title}</h1>
 
         {authStatus && (
-          <button className="property__bookmark-button button" type="button">
-            <svg className="property__bookmark-icon" width={31} height={33}>
+          <button onClick={() => {
+            dispatch(Operation.toggleIsFavorite(id, status))
+          }} className={`property__bookmark-button ${isFavorite && `property__bookmark-button--active`} button`} type="button">
+            <svg className="place-card__bookmark-icon" width={31} height={33}>
               <use xlinkHref="#icon-bookmark" />
             </svg>
             <span className="visually-hidden">To bookmarks</span>
